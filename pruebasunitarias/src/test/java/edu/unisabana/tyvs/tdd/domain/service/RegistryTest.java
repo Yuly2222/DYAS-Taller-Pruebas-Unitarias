cfd9a8016b7e94a1d410e865c68a273236607b36
Yuly :) <159471511+Yuly2222@users.noreply.github.com>
2026-04-23 11:01:47 -0500

    Mejora en pruebas unitarias

--- pruebasunitarias/src/test/java/edu/unisabana/tyvs/tdd/domain/service/RegistryTest.java
+++ pruebasunitarias/src/test/java/edu/unisabana/tyvs/tdd/domain/service/RegistryTest.java
@@ -1,76 +1,105 @@
 package edu.unisabana.tyvs.domain.service;
 
-import edu.unisabana.tyvs.domain.model.*;
+import edu.unisabana.tyvs.domain.model.Gender;
+import edu.unisabana.tyvs.domain.model.Person;
+import edu.unisabana.tyvs.domain.model.RegisterResult;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class RegistryTest {
 
     @Test
-    public void shouldRejectDeadPerson() {
+    public void shouldRejectNullPerson() {
         Registry registry = new Registry();
-        Person dead = new Person("Carlos", 2, 40, Gender.MALE, false);
-        RegisterResult result = registry.registerVoter(dead);
-        Assert.assertEquals(RegisterResult.DEAD, result);
+
+        RegisterResult result = registry.registerVoter(null);
+
+        Assert.assertEquals(RegisterResult.INVALID, result);
     }
 
     @Test
-    public void shouldRejectUnderageAt17() {
+    public void shouldRejectDeadPerson() {
         Registry registry = new Registry();
-        Person person = new Person("Ana", 1, 17, Gender.FEMALE, true);
-        RegisterResult result = registry.registerVoter(person);
-        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+        Person dead = new Person("Carlos", 2, 40, Gender.MALE, false);
+
+        RegisterResult result = registry.registerVoter(dead);
+
+        Assert.assertEquals(RegisterResult.DEAD, result);
     }
 
     @Test
     public void shouldRejectNegativeAge() {
         Registry registry = new Registry();
         Person person = new Person("Ana", 1, -5, Gender.FEMALE, true);
+
         RegisterResult result = registry.registerVoter(person);
+
         Assert.assertEquals(RegisterResult.INVALID_AGE, result);
-    }
-
-    @Test
-    public void shouldRejectNullPerson() {
-        Registry registry = new Registry();
-        RegisterResult result = registry.registerVoter(null);
-        Assert.assertEquals(RegisterResult.INVALID, result);
-    }
-
-    @Test
-    public void shouldAllowAdultAt18() {
-        Registry registry = new Registry();
-        Person person = new Person("Luis", 3, 18, Gender.MALE, true);
-        RegisterResult result = registry.registerVoter(person);
-        Assert.assertEquals(RegisterResult.VALID, result);
     }
 
     @Test
     public void shouldRejectAgeOver120() {
         Registry registry = new Registry();
-        Person person = new Person("Pedro", 4, 121, Gender.MALE, true);
+        Person person = new Person("Pedro", 3, 121, Gender.MALE, true);
+
         RegisterResult result = registry.registerVoter(person);
+
         Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+    }
+
+    @Test
+    public void shouldRejectUnderageAt17() {
+        Registry registry = new Registry();
+        Person person = new Person("Laura", 4, 17, Gender.FEMALE, true);
+
+        RegisterResult result = registry.registerVoter(person);
+
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+
+    @Test
+    public void shouldAllowAdultAt18() {
+        Registry registry = new Registry();
+        Person person = new Person("Luis", 5, 18, Gender.MALE, true);
+
+        RegisterResult result = registry.registerVoter(person);
+
+        Assert.assertEquals(RegisterResult.VALID, result);
+    }
+
+    @Test
+    public void shouldAllowValidAge120() {
+        Registry registry = new Registry();
+        Person person = new Person("Marta", 6, 120, Gender.FEMALE, true);
+
+        RegisterResult result = registry.registerVoter(person);
+
+        Assert.assertEquals(RegisterResult.VALID, result);
     }
 
     @Test
     public void shouldRejectDuplicatedId() {
         Registry registry = new Registry();
 
-        Person p1 = new Person("Ana", 1, 25, Gender.FEMALE, true);
-        Person p2 = new Person("Laura", 1, 30, Gender.FEMALE, true);
+        Person first = new Person("Ana", 7, 25, Gender.FEMALE, true);
+        Person duplicate = new Person("Maria", 7, 30, Gender.FEMALE, true);
 
-        registry.registerVoter(p1);
-        RegisterResult result = registry.registerVoter(p2);
+        registry.registerVoter(first);
+        RegisterResult result = registry.registerVoter(duplicate);
 
         Assert.assertEquals(RegisterResult.DUPLICATED, result);
     }
 
     @Test
-    public void shouldAllowValidPerson() {
+    public void shouldNotStoreIdWhenAgeIsInvalid() {
         Registry registry = new Registry();
-        Person person = new Person("Mario", 5, 30, Gender.MALE, true);
-        RegisterResult result = registry.registerVoter(person);
+
+        Person invalid = new Person("Ana", 8, -1, Gender.FEMALE, true);
+        Person validLater = new Person("Ana", 8, 25, Gender.FEMALE, true);
+
+        registry.registerVoter(invalid);
+        RegisterResult result = registry.registerVoter(validLater);
+
         Assert.assertEquals(RegisterResult.VALID, result);
     }
 }
